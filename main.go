39b10fc2f7d5280c5593b6a187ac5d3e55ba44fe
Erencan Çabuk <erncncbk@gmail.com>
2022-02-07 15:49:21 +0300

    gomock

--- main.go
+++ main.go
@@ -6,24 +6,25 @@ import (
 
 	"github.com/erncncbk/simplebank/api"
 	db "github.com/erncncbk/simplebank/db/sqlc"
+	"github.com/erncncbk/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:6785@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load congig: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
